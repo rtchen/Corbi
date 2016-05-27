b6ae564b323e6f6b55af2db8e80c3333171ddbb9
rtchen <ruitangchen@163.com>
2016-05-28 07:55:56 +0800

    still has major bug, difference is nan when edges are too many???

--- net_align.R
+++ net_align.R
@@ -6,7 +6,8 @@ net_align <- function(netfileA, netfileB,simfile, alpha=1,beta=2,delta.d=1e-10,o
   crf <-build_model(combined_net,alpha,beta,delta.d)
   c <- Sys.time()
   print(c-b)
-  result <- decode.lbp(crf)
+  gc()
+  result <- decode.lbp(crf,max.iter=1000,verbose=1)
   d <- Sys.time()
   result <- crf$state.map[cbind(1:crf$n.nodes, result)]
   print(d-c)
@@ -22,28 +23,34 @@ read_net <- function(netfileA,netfileB,simfile)
   net.nodeA <- unique(as.character(net.textA))
   net.nodeA <- net.nodeA[net.nodeA != ""]
   net.sizeA <- length(net.nodeA)
+  net.nodeA <- paste("A_",sep="",net.nodeA)
   net.nodeB <- unique(as.character(net.textB))
   net.sizeB <- length(net.nodeB)
+  net.nodeB <-paste("B_",sep="",net.nodeB)
   net.nodeB <- net.nodeB[net.nodeB != ""]
-  sim.node <- unique(c(as.character(sim.text[,1]),as.character(sim.text[,2])))
+  sim.node <- unique(c(paste("A_",sep="",as.character(sim.text[,1])),paste("B_",sep="",as.character(sim.text[,2]))))
   sim.node <- sim.node[sim.node !=""]
-  net.node <- unique(c(net.nodeA,net.nodeB,sim.node))
-  net.size = length(net.node)
-  print(net.size)
-  net.edgeA <- cbind(as.character(net.textA[,1]), as.character(net.textA[,2]))
-  net.edgeB <- cbind(as.character(net.textB[,1]), as.character(net.textB[,2]))
-  net.edgeA <- net.edgeA[net.edgeA[,2] != "", ]
-  net.edgeB <- net.edgeB[net.edgeB[,2] != "", ]
-  sim.edge <- cbind(as.character(sim.text[,1]), as.character(sim.text[,2]))
+  net.node <- unique(c(net.nodeA,net.nodeB))
+  net.sizeAB <- length(net.node)
+  net.node <- unique(c(net.node, sim.node))
+  net.size <- length(net.node)
+  net.edgeA <- cbind(paste("A_",sep="",as.character(net.textA[,1])), paste("A_",sep="",as.character(net.textA[,2])))
+  net.edgeB <- cbind(paste("B_",sep="",as.character(net.textB[,1])), paste("B_",sep="",as.character(net.textB[,2])))
+  net.edgeA <- net.edgeA[net.edgeA[,2] != "A_", ]
+  net.edgeB <- net.edgeB[net.edgeB[,2] != "B_", ]
+  sim.edge <- cbind(paste("A_",sep="",as.character(sim.text[,1])), paste("B_",sep="",as.character(sim.text[,2])))
+  sim.edgerev <- cbind(paste("B_",sep="",as.character(sim.text[,2])), paste("A_",sep="",as.character(sim.text[,1])))
+  sim.edge <- rbind(sim.edge,sim.edgerev)
   net.edge <- rbind(net.edgeA,sim.edge,net.edgeB)
   node.id <- seq_along(net.node)
   names(node.id) <- net.node
   net.matrixA <- sparseMatrix(node.id[net.edgeA[,1]], node.id[net.edgeA[,2]], x=T, dims=c(net.size+1, net.size+1))
   net.matrixB <- sparseMatrix(node.id[net.edgeB[,1]], node.id[net.edgeB[,2]], x=T, dims=c(net.size+1, net.size+1))
-  sim.matrix <- sparseMatrix(node.id[sim.edge[,1]], node.id[sim.edge[,2]], x=as.numeric(sim.text[,3]), dims=c(net.size, net.size))
+  sim.matrix <-  sparseMatrix(node.id[sim.edge[,1]], node.id[sim.edge[,2]], x=as.numeric(sim.text[,3]), dims=c(net.size, net.size))
   net.matrix <- sparseMatrix(node.id[net.edge[,1]], node.id[net.edge[,2]], x=T, dims=c(net.size, net.size))
+  sim.edge <- cbind(node.id[sim.edge[,1]],node.id[sim.edge[,2]],as.numeric(sim.text[,3]))
   list(node_sim = sim.matrix,sizeA=net.sizeA,node=net.node, matrix=net.matrix,size = net.size,
-       matrixA = net.matrixA,matrixB = net.matrixB)
+       matrixA = net.matrixA,matrixB = net.matrixB,sim_edge = sim.edge,sizeAB = net.sizeAB)
   
 }
 
@@ -52,29 +59,39 @@ build_model <- function(combined_net,alpha,beta,delta.d)
   S <- cbind(combined_net$node_sim,delta.d)
   crf <- make.crf(combined_net$matrix, rowSums(S>0)) 
   crf$state.map <- matrix(0, nrow=crf$n.nodes, ncol=crf$max.state)
-  print("here1")
-  for (i in 1:crf$n.nodes)
-  { print(i)
-    crf$state.map[i, 1:crf$n.states[i]] <- which(S[i,] > 0)
-    crf$node.pot[i, 1:crf$n.states[i]] <- exp(S[i, crf$state.map[i,]]*alpha/2)
+  edges <- combined_net$sim_edge
+  state_cnt_vec <- vector('integer',crf$n.nodes)
+  len <- dim(edges)[1]
+  for( edge in 1:len){
+    i <- edges[edge,1]
+    j <- edges[edge,2]
+    x <- edges[edge,3]
+    state_cnt_vec[i] <- state_cnt_vec[i] + 1
+    crf$state.map[i,state_cnt_vec[i]] <- j
+    crf$node.pot[i,state_cnt_vec[i]] <- exp(x*alpha/2)
   }
-  A.size = combined_net$sizeA
+  for(i in 1:crf$n.nodes){
+    state_cnt_vec[i] <- state_cnt_vec[i] + 1
+    crf$state.map[i,state_cnt_vec[i]] <- combined_net$size+1
+    crf$node.pot[i,state_cnt_vec[i]] <- 1
+  }
+  A.size <- combined_net$sizeA
+  AB.size <- combined_net$sizeAB
   W1 <-  combined_net$matrixA
   W2 <- combined_net$matrixB
-  print("here2")
   for (e in 1:crf$n.edges)
-  { a<-Sys.time()
+  { 
     n1 <- crf$edges[e, 1]
     n2 <- crf$edges[e, 2]
     m1 <- 1:crf$n.states[n1]
     m2 <- 1:crf$n.states[n2]
-    if(n1<=A.size && n2 <=A.size){
-    W <- submatrix(W2,crf$state.map[n1, m1],crf$state.map[n2, m2])
-    crf$edge.pot[[e]] <- exp(W*beta/2)
+    if(n1<=A.size && n2 <=A.size && n1 <=AB.size && n2 <=AB.size){
+     W <- submatrix(W2,crf$state.map[n1, m1],crf$state.map[n2, m2])
+     crf$edge.pot[[e]] <- exp(W*beta/2)
     }
-    else if(n1 > A.size && n2 > A.size){
-    W <- submatrix(W1,crf$state.map[n1, m1],crf$state.map[n2, m2])
-    crf$edge.pot[[e]] <- exp(W*beta/2)
+    else if(n1 > A.size && n2 > A.size  && n1 <=AB.size && n2 <=AB.size){
+     W <- submatrix(W1,crf$state.map[n1, m1],crf$state.map[n2, m2])
+     crf$edge.pot[[e]] <- exp(W*beta/2)
     }
     else{
       G <- matrix(1,nrow=length(m1),ncol = length(m2))
@@ -95,15 +112,15 @@ write_result <- function(combined_net, result, filename="result.txt")
 {   
   con <- file(as.character(filename), "w")
   label.name <- c(combined_net$node, "gap")
-  cnt <- 0
+ # cnt <- 0
   for (i in 1:combined_net$sizeA)
-  { if (result[i] <= combined_net$size){
-    cnt <- cnt+1
-     }
+  { #if (result[i] <= combined_net$size){
+    #cnt <- cnt+1
+  #}
     writeLines(paste(combined_net$node[i], "  ", label.name[result[i]]), con, sep="\n")
   }
   writeLines("", con, sep="\n")
-  print(cnt)
+  #print(cnt)
   close(con)
 }
 
